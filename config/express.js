47df3e5b476ba4b9a0266e5df2d542876be10844
ttrmoreira <ttrmoreira@gmail.com>
2019-01-29 10:18:59 -0200

    Inclusão da API BodyParser

--- config/express.js
+++ config/express.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var consign = require('consign');
+var bodyParser = require('body-parser');
 
 module.exports = function(){
+    
+
     var app = express();
     
     app.set('port',3000);
     app.use(express.static('./public'));
     app.set('view engine','ejs');
     app.set('views', './app/views');
+    app.use(bodyParser.urlencoded({extended: true}));
     
     consign({cwd: 'app'})
         .include('models')
